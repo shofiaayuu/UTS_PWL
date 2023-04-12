2e7cac1ff87a69e7e21aa0ad3509c40b558e23cb
Shofiatul Ayu Anikaningrum <shofiaa060@gmail.com>
2023-04-12 10:26:27 +0700

    Membuat Authentication

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::middleware(['auth'])->group(function(){
 
     Route::get('/logout', [LoginController::class, 'logout']);
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
